07a973cbd713c7f66a5d502b25c4fc063ef6d79b
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-09-04 11:23:45 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accId = 4356
+let accEmail = "basirahmadmalik@gmail.com"
+var accPassword = "123456"
+accCity = "Delhi"
+
+// accId = 4
+// console.log(accId);
+
+accEmail = "Ahmad@gmail.com"
+accPassword = "123"
+accCity = 'Aligarh'
+
+console.table([accId, accEmail, accPassword, accCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+let name = "Basir"
+let num = 12.00
+let state;
+
+console.log(typeof name); // string
+console.log(typeof num);  // number
+console.log(typeof null); // object
+console.log(typeof state); // Undefined
+console.log(typeof undefined); // Undefined
