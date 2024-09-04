311e9a4a3c18f5d564fc3b514ee46c05d45ec81e
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-09-04 13:38:21 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+/*** Conversion to Number ***/
+
+// let score = undefined
+// // console.log(typeof score)
+
+// let newScore = Number(score) 
+
+// console.log(newScore);
+
+
+/*** Conversion to Boolean ***/
+
+// let score = 0
+// let score = "Basir"
+// let score = 23
+// let score = null
+// let score = undefined
+
+// let newScore = Boolean(score)
+
+// console.log(newScore);
+
+/*** Conversion to String ***/
+
+// let score = 18
+// let score = null
+let score = undefined
+
+let newScore = String(score)
+
+console.log(newScore);
