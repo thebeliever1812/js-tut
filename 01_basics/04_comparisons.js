5954a74e40ac1d8eb62b38bde57b485eccf7d343
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-09-07 16:11:01 +0000

    datatypes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null > 0); // in javascript when we compare null it will converted into a number, treating as 0
+
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+console.log("2" === 2); // "==" it checks without datatype....Conversion comes into play
+// "===" it checks value with datatype
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,16 @@
+/** Primitive */
+
+// 7 types: Number, String, Boolean, Symbol, null, undefined, BigInt
+
+const score = 100
+const scoreValue = 100.5
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const newId = Symbol('123')
+
+const bigNum = 143534567n
+
+console.log(id === newId);
