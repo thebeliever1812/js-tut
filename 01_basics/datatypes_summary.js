4e06c355c9f3d303f9ae4b314d74019e56b6f732
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-09-09 07:21:14 +0000

    datatype summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -2,15 +2,31 @@
 
 // 7 types: Number, String, Boolean, Symbol, null, undefined, BigInt
 
-const score = 100
-const scoreValue = 100.5
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const score = 100
+// const scoreValue = 100.5
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const newId = Symbol('123')
+// const id = Symbol('123')
+// const newId = Symbol('123')
 
-const bigNum = 143534567n
+// const bigNum = 143534567n
 
-console.log(id === newId);
+// console.log(id === newId);
+
+/*** Reference (Non-primitive) */
+
+// array, objects, functions
+
+const myhero =["Captain America", "Thor", "Iron Man"] // array
+
+const myObj = {name: "Basir", age: 22, gender: "male"} // Objects
+
+const myfunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof myhero); // object
+console.log(typeof myObj); // object
+console.log(typeof myfunction); // type -> Function... but we usaslly say object function
