1d05afd58b1112eea6cc778f1c1afd5b6374456f
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-09-09 14:23:07 +0000

    stack and heap done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -19,14 +19,41 @@
 
 // array, objects, functions
 
-const myhero =["Captain America", "Thor", "Iron Man"] // array
+// const myhero =["Captain America", "Thor", "Iron Man"] // array
 
-const myObj = {name: "Basir", age: 22, gender: "male"} // Objects
+// const myObj = {name: "Basir", age: 22, gender: "male"} // Objects
 
-const myfunction = function() {
-    console.log("Hello World");
+// const myfunction = function() {
+//     console.log("Hello World");
+// }
+
+// console.log(typeof myhero); // object
+// console.log(typeof myObj); // object
+// console.log(typeof myfunction); // type -> Function... but we usaslly say object function
+
+/******* Stack memory (Primitive) & Heap memory (Non-primitive ) */
+
+/** Example of stack */
+
+// let person1 = "Basir"
+// let person2 = person1
+
+// person2 = "Virat"
+
+// console.log(person1);
+// console.log(person2);
+
+/** Example of Heap */
+
+let userOne = {
+    email: "userone@gmail.com",
+    age: 22
 }
 
-console.log(typeof myhero); // object
-console.log(typeof myObj); // object
-console.log(typeof myfunction); // type -> Function... but we usaslly say object function
+let userTwo = userOne
+
+userTwo.email = "usertwo@gmail.com"
+userTwo.age = 33
+console.log(userOne.email);
+console.log(userTwo.email);
+console.log(userOne.age);
