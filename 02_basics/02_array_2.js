efcc9819ebcadfe3ac919352521f80803be10d09
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-10-10 06:41:15 +0000

    array

---
+++ 02_basics/02_array_2.js
@@ -0,0 +1,10 @@
+const marvel = ["thor", "Cap", "Hulk"]
+const dc = ["flash", "Batman", "Superman"]
+
+// marvel.push(dc)
+
+// console.log(marvel);
+
+const allHeroes = marvel.concat(dc) // Concatinates two array and return a new array
+
+console.log(allHeroes);
