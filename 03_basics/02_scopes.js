c5bbefb68cae397fe55eec07b819a336f98e7231
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-10-14 06:17:27 +0000

    hoisting done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,24 @@ if (true) {
   let a = 10;
   const b = 20;
   //   var c = 30; // it has scope inside and outside the loop, so it is not prefer to use "var" to declare any variable
-  console.log("Inner value of a: ", a);
+  //   console.log("Inner value of a: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c); // We have two values for c 300 and 30 it will print 30 which is inside the "if"
+
+/******* Hoisting */
+
+console.log(addone(5)); // Accessing function before its declaration is known as Hoisting
+
+function addone(num) {
+  return num + 1;
+}
+
+addTwo(5);
+
+// another way of function declaration
+const addTwo = function (num) {
+  return num + 2;
+};
