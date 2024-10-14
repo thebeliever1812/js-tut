9aa8220028e7f42612b17c906ad3f5e918a488f1
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-10-14 05:46:55 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,12 @@
+// var c = 300
+let a = 100;
+if (true) {
+  let a = 10;
+  const b = 20;
+  //   var c = 30; // it has scope inside and outside the loop, so it is not prefer to use "var" to declare any variable
+  console.log("Inner value of a: ", a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c); // We have two values for c 300 and 30 it will print 30 which is inside the "if"
