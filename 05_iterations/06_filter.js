8182dd53942ce46ae421c0fe72ae26c12bba1d30
thebeliever1812 <127594864+thebeliever1812@users.noreply.github.com>
2024-10-23 04:37:10 +0000

    filter

--- 05_iterations/06_filter.js
+++ 05_iterations/06_filter.js
@@ -9,13 +9,44 @@
 
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// const newNums = num.filter((val) => val < 5);
+// const newNums = num.filter((val) => val < 5); // no return keywod is required in this case bcz we did not used curly braces here
 
 // console.log(newNums);
 
 // if we use curly braces then we have to use the return ketword in filter
-const newNums = num.filter((val) => {
-  return val < 7; 
-});
+// const newNums = num.filter((val) => {
+//   return val < 7;
+// });
 
-console.log(newNums);
+// console.log(newNums);
+
+// Same thing done by using for-each loop
+// const mynum = [];
+
+// num.forEach((val) => {
+//   if (val < 6) {
+//     mynum.push(val);
+//   }
+// });
+
+// console.log(mynum);
+
+// const books = [
+//   { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+//   { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+//   { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+//   { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+//   { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+//   { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+//   { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+//   { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+//   { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+// ];
+
+// let user = books.filter((bk) => bk.genre === "History");
+
+// user = books.filter((books) => {
+//   return books.publish >= 1995 && books.genre === "History";
+// });
+
+// console.log(user);
